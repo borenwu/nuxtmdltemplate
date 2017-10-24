0cca38cc41da2207188fd9fa11463cb1efe4ae98
borenwu <borenwuapple@163.com>
2017-10-24 16:25:08 +0800

    fourth

--- server/api/blogs.js
+++ server/api/blogs.js
@@ -5,57 +5,57 @@ const router = Router()
 // Mock blogs
 const blogs = [
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work01.jpg',
-        title:'Blog template',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work07.jpg',
-        title:'Sunt nulla',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work02.jpg',
-        title:'Android.com website',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work03.jpg',
-        title:'Dashboard template',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work04.jpg',
-        title:'Text-heavy website',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work08.jpg',
-        title:'Ex officia laborum',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work05.jpg',
-        title:'Stand-alone article',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work06.jpg',
-        title:'MDL website',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
     {
-        img: 'http://oy988o88l.bkt.clouddn.com/example-work09.jpg',
-        title:'Consequat ut quis',
-        shortContent:'Enim labore aliqua consequat ut quis ad occaecat aliquip incididunt. Sunt nulla eu enim irure enim nostrud aliqua consectetur ad consectetur sunt ullamco officia. Ex officia laborum et consequat duis.'
+        img: 'http://oybhwbo4h.bkt.clouddn.com/DK003/DK003_01.jpg',
+        title:'UGG DK 003',
+        shortContent:'DK UGG DK003 防泼水雪地靴'
     },
 
 ]
